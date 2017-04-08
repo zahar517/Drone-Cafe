7b4242c9ec1fcaba99623276033c75b0bbc22bcd
zahar517 <5173428@mail.ru>
2017-04-08 08:18:56 +0300

    Add tests for Login Form

--- e2e-tests/index.spec.js
+++ e2e-tests/index.spec.js
@@ -1,6 +1,60 @@
-describe('Main page test', () => {
+describe('Main page', () => {
   it('should load angularjs', () => {
     browser.get('http://localhost:8000/');
     expect(browser.getCurrentUrl()).toBe('http://localhost:8000/');
+  });
+
+  describe('Login form', () => {
+    let userName, userEmail, submitButton;
+
+    beforeEach(() => {
+      browser.get('http://localhost:8000/');
+      userName = element(by.model('$ctrl.newUser.name'));
+      userEmail = element(by.model('$ctrl.newUser.email'));
+      submitButton = element(by.css('.md-primary'));
+    });
+
+    it('should be present', () => {
+      expect($('form').isPresent()).toBeTruthy();
+    });
+
+    it('submitButton should be disabled if all fileds are empty', function() {
+      userEmail.clear();
+      userEmail.sendKeys('');
+      userName.clear();
+      userName.sendKeys('');
+
+      expect(submitButton.getAttribute('disabled')).toEqual('true');
+    });
+
+    it('submitButton should be disabled if empty user name', function() {
+      userEmail.sendKeys('abc@ya.ru');
+      userName.clear();
+      userName.sendKeys('');
+
+      expect(submitButton.getAttribute('disabled')).toEqual('true');
+    });
+
+    it('submitButton should be disabled if empty user email', function() {
+      userName.sendKeys('user1');
+      userEmail.clear();
+      userEmail.sendKeys('');
+
+      expect(submitButton.getAttribute('disabled')).toEqual('true');
+    });
+
+    it('submitButton should be disabled if user name filled and bad email', function() {
+      userEmail.sendKeys('abc');
+      userName.sendKeys('user1');
+
+      expect(submitButton.getAttribute('disabled')).toEqual('true');
+    });
+
+    it('submitButton should not be disabled if user name and email filled with good data', function() {
+      userEmail.sendKeys('abc@ya.ru');
+      userName.sendKeys('user1');
+
+      expect(submitButton.getAttribute('disabled')).toBeNull();
+    });
   });
 });
