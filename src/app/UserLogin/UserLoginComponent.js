9334d34705b09e99c0f5c392a4a283d2018f47a7
zahar517 <5173428@mail.ru>
2017-04-09 14:53:50 +0300

    Add default value for newUser

--- src/app/UserLogin/UserLoginComponent.js
+++ src/app/UserLogin/UserLoginComponent.js
@@ -5,6 +5,7 @@ angular.module('DroneCafe').component('userLogin', {
   templateUrl: '/app/UserLogin/UserLogin.html',
 
   controller: function () {
+    this.newUser = null;
     this.checkUser = newUser => newUser;
     this.title = 'Drone Cafe';
   }
