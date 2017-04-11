124e61f75688d636956590011cb58f1dc5b451d6
zahar517 <5173428@mail.ru>
2017-04-11 12:00:53 +0300

    Add some tests

--- src/app/CustomerMenu/CustomerMenu.spec.js
+++ src/app/CustomerMenu/CustomerMenu.spec.js
@@ -13,4 +13,13 @@ describe('Drone Cafe Customer Menu module', function() {
     expect(controller.title).toBeDefined();
     expect(controller.dishes).toBeDefined();
   });
+
+  it('it should have empty properties after init', function() {
+    let length = controller.dishes.length;
+    expect(length).toBe(0);
+  });
+
+  it('it should have title "Choose dish"', function() {
+    expect(controller.title).toBe('Choose dish');
+  });  
 });
