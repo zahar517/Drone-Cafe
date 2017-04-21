4b4ccdc9a6b5f0837d9b4f772348801c2b06d4c9
zahar517 <5173428@mail.ru>
2017-04-21 20:55:57 +0300

    Add realtime func to Kitchen

--- src/app/DishCookingItem/DishCookingItemComponent.js
+++ src/app/DishCookingItem/DishCookingItemComponent.js
@@ -16,7 +16,15 @@ angular.module('DroneCafe.DishCookingItem', [])
       const newState = { state: 3 };
 
       kitchenService.changeState(dish._id, newState)
-        .then(data => kitchenService.getOrder())
+        .then(data => {
+
+          mySocket.emit('send message to', {
+            room: `user${dish._user}`,
+            dish,
+          });
+
+          return kitchenService.getOrder()
+        })
         .then(res => this.order = res.data)
         .catch(err => console.log(err));
 
--- src/app/Kitchen/KitchenComponent.js
+++ src/app/Kitchen/KitchenComponent.js
@@ -8,8 +8,17 @@ angular.module('DroneCafe.Kitchen', [])
     order: '<'
   },
 
-  controller: function () {
+  controller: function (mySocket, kitchenService) {
   
+    mySocket.emit('subscribe', 'kitchen')
+
+    mySocket.on('changeState', (data) => {
+      console.log('Changed state ', data);
+      kitchenService.getOrder()
+        .then(res => this.order = res.data)
+        .catch(err => console.log(err))
+    });
+
   },
 
   templateUrl: '/app/Kitchen/Kitchen.html',
