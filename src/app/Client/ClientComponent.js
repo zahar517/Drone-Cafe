7730930078d2aa8089ab827e7161bb4887626b5b
zahar517 <5173428@mail.ru>
2017-04-21 19:53:47 +0300

    Add realtime func to client

--- src/app/Client/ClientComponent.js
+++ src/app/Client/ClientComponent.js
@@ -9,7 +9,19 @@ angular.module('DroneCafe.Client', [])
     order: '<',
   },
 
+  controller: function (authService, mySocket, menuService) {
+
+    mySocket.emit('subscribe', `user${authService.getUserId()}`)
+
+    mySocket.on('changeState', (data) => {
+      console.log('Changed state ', data);      
+      menuService.getOrder()
+        .then(res => this.order = res.data)
+        .catch(err => console.log(err));
+    });
+
+  },
+
   templateUrl: '/app/Client/Client.html',
 
-  controller: function () {}
 });
--- src/app/CustomerMenuItem/CustomerMenuItemComponent.js
+++ src/app/CustomerMenuItem/CustomerMenuItemComponent.js
@@ -9,13 +9,26 @@ angular.module('DroneCafe.CustomerMenuItem', [])
     dish: '<'
   },
 
-  controller: function(menuService, userService) {
+  controller: function(menuService, userService, authService, mySocket) {
 
     this.makeOrder = (dish) => {
 
       menuService.makeOrder(dish)
         .then(res => userService.getUser())
         .then(res => this.user = res.data)
+        .then(() => {
+
+          mySocket.emit('send message to', {
+            room: `user${authService.getUserId()}`,
+            dish,
+          });
+
+          mySocket.emit('send message to', {
+            room: 'kitchen',
+            dish,
+          });
+
+        })
         .catch(err => console.log(err));
     };
 
